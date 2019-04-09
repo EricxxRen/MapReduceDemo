09b0381793e833ab2355392a45cdbbad093d08c8
Eric Ren <ericzjhz@gmail.com>
2019-04-09 18:12:33 +0800

    added posts and comments hierarchy demo

--- src/main/java/com/eki/mapreduce/ch4/PostCommentDriver.java
+++ src/main/java/com/eki/mapreduce/ch4/PostCommentDriver.java
@@ -1,8 +1,83 @@
 package com.eki.mapreduce.ch4;
 
+import com.eki.mapreduce.util.MRDPUtil;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class PostCommentDriver {
 
+    public static class PostMapper extends Mapper<Object, Text, Text, Text> {
+        private Text outKey = new Text();
+        private Text outValue = new Text();
 
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            Map<String, String> parsed = MRDPUtil.transformXml2Map(value.toString());
+            String postID = parsed.get("ID");
+
+            if (postID == null) {
+                return;
+            }
+
+            outKey.set(postID);
+            outValue.set("P" + value.toString());
+            context.write(outKey, outValue);
+        }
+    }
+
+    public static class CommentMapper extends Mapper<Object, Text, Text, Text> {
+        private Text outKey = new Text();
+        private Text outValue = new Text();
+
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            Map<String, String> parsed = MRDPUtil.transformXml2Map(value.toString());
+            String postID = parsed.get("postID");
+
+            if (postID == null) {
+                return;
+            }
+
+            outKey.set(postID);
+            outValue.set("C" + value.toString());
+            context.write(outKey, outValue);
+        }
+    }
+
+    public static class PCReducer extends Reducer<Text, Text, Text, NullWritable> {
+        private String post = null;
+        private ArrayList<String> comments = new ArrayList<String>();
+        private DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            post = null;
+            comments.clear();
+
+            for (Text value : values) {
+                if (value.charAt(0) == 'P') {
+                    post = value.toString().substring(1, value.toString().length()).trim();
+                } else {
+                    comments.add(value.toString().substring(1, value.toString().length()).trim());
+                }
+            }
+
+            if (post != null) {
+
+            }
+        }
+
+        private String nestElements (DocumentBuilderFactory dbf, String post, ArrayList<String> comments) {
+            return null;
+        }
+    }
 
     public static void main(String[] args) {
 
--- src/main/java/com/eki/mapreduce/util/MRDPUtil.java
+++ src/main/java/com/eki/mapreduce/util/MRDPUtil.java
@@ -1,5 +1,21 @@
 package com.eki.mapreduce.util;
 
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,4 +36,40 @@ public class MRDPUtil {
         }
         return map;
     }
+
+    public static Element getXmlElementFromString (DocumentBuilderFactory dbf, String xml) {
+        try {
+            DocumentBuilder builder = dbf.newDocumentBuilder();
+
+            return builder.parse(new InputSource(new StringReader(xml))).getDocumentElement();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void copyAttributesToElement (NamedNodeMap attributes, Element element) {
+        for (int i = 0; i < attributes.getLength(); ++i) {
+            Attr toCopy = (Attr) attributes.item(i);
+            element.setAttribute(toCopy.getName(), toCopy.getValue());
+        }
+    }
+
+    public static String transformDocumentToString (Document document) {
+        try {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(writer));
+
+            return writer.getBuffer().toString().replaceAll("\n|\r", "");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
